463fff4134fd74b7b84ef8911c0aaa5cdf1dad36
mmxyh <miaomiaoxiyh@163.com>
2020-09-27 14:34:08 +0800

    foodie-dev： 1

---
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: hmm
+ * @date: 2020/9/27 13:49
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.imooc.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: hmm
+ * @date: 2020/9/27 13:51
+ */
+//@Controller
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Object hello(){
+        return "hello World！！";
+    }
+}
